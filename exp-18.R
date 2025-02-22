071eb818872981528fdf74747ef3ba4906ee3442
Somasekharparlapalli <128777311+Somasekharparlapalli@users.noreply.github.com>
2025-02-22 14:27:20 +0530

    Delete exp-18.R

--- exp-18.R
+++
@@ -1,4 +0,0 @@
-data("mtcars")
-boxplot(mpg ~ cyl, data=mtcars, col=c("lightblue", "lightgreen", "lightcoral"),
-        main="MPG vs Number of Cylinders",
-        xlab="Number of Cylinders", ylab="Miles Per Gallon (MPG)")
